0115d39c311ea9d5d6a78a4c59c16e988b042c03
Your Name <you@example.com>
2024-04-29 00:21:08 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/gluttonie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gluttonie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:gluttonie/categories.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final theme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+        brightness: Brightness.dark,
+        seedColor: const Color.fromARGB(255, 143, 27, 19)));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
